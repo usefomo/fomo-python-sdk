aed2b8d7c4d97ad8e66bd92e829160b8089617f8
Chris Bacon <baconck@gmail.com>
2018-12-19 14:58:29 -0700

    bump version to 1.2.0

--- Fomo/__init__.py
+++ Fomo/__init__.py
@@ -4,4 +4,4 @@ from .fomo import (
     FomoEventBasic
 )
 
-__version__ = '1.1.0'
+__version__ = '1.2.0'
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='fomo',
-    version='1.1.0',
+    version='1.2.0',
 
     description='Fomo Python SDK is the official SDK wrapper for the Fomo API service',
     long_description=long_description,
